7b9687c08969ef569302cbd272cd7e58367eb921
Pawel <p.kapica6@gmail.com>
2021-10-27 11:00:53 +0200

    add login and register

---
+++ app/src/main/java/com/example/tripogranizer/LoginActivity.java
@@ -0,0 +1,76 @@
+package com.example.tripogranizer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailET;
+    private EditText passwordET;
+    private Button submitBTN;
+    private Button createAccountBTN;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailET = findViewById(R.id.login_edit_email);
+        passwordET = findViewById(R.id.login_edit_password);
+        submitBTN = findViewById(R.id.login_btn_submit);
+        createAccountBTN = findViewById(R.id.login_btn_createAccount);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        createAccountBTN.setOnClickListener(v-> {
+            startActivity(new Intent(LoginActivity.this , RegisterActivity.class));
+        });
+        submitBTN.setOnClickListener(v-> {
+            String txt_email = emailET.getText().toString();
+            String txt_password = passwordET.getText().toString();
+
+            if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+                Toast.makeText(LoginActivity.this, "Wypełnij wszystkie pola!", Toast.LENGTH_SHORT).show();
+            } else {
+                loginUser(txt_email , txt_password);
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Dane prawidłowe", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this , MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/tripogranizer/MainActivity.java
+++ app/src/main/java/com/example/tripogranizer/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.tripogranizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(MainActivity.this , LoginActivity.class));
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null){
+            Toast.makeText(MainActivity.this,"ZALOGOWANY",Toast.LENGTH_LONG);
+
+        } else {
+            Toast.makeText(MainActivity.this,"NIE ZALOGOWANY",Toast.LENGTH_LONG);
+        }
     }
 }
